9155f8fbb92d60d1e6998a334453ba02bec23a23
Beinsezii <beinsezii@gmail.com>
2022-09-08 23:34:07 -0700

    EGUI Mostly done themeing
     * colors work how I want
     * can even change sizes
    
     * TODO if buttons are too small for text it breaks shit
     * TODO the button borders are inconsistent when hovering with mouse
       * https://github.com/emilk/egui/discussions/2032

--- src/front_egui.rs
+++ src/front_egui.rs
@@ -2,9 +2,10 @@ use super::ButtSets;
 use eframe::{
     egui::{
         self,
-        style::{WidgetVisuals, Widgets},
+        style::{Margin, Spacing, WidgetVisuals, Widgets},
         Button, CentralPanel, Color32, Context, Grid, Key, Stroke, Style, Visuals,
     },
+    epaint::Rounding,
     App, Frame,
 };
 
@@ -22,7 +23,7 @@ impl App for ButtBox {
                     .outer_margin(0.0),
             )
             .show(ctx, |ui| {
-                Grid::new("Butts").spacing((1.0, 1.0)).show(ui, |ui| {
+                Grid::new("Butts").show(ui, |ui| {
                     let mut run = None;
 
                     for (n, b) in self.butts.commands.iter().enumerate() {
@@ -63,15 +64,122 @@ impl App for ButtBox {
 
 impl ButtBox {
     pub fn new(cc: &eframe::CreationContext<'_>, butts: ButtSets) -> Self {
+        // General Style
+        let rounding = Rounding::none();
+        let bg_stroke = Stroke::none();
+        let style = cc.egui_ctx.style().as_ref().clone();
+        cc.egui_ctx.set_style(Style {
+            visuals: Visuals {
+                widgets: Widgets {
+                    active: WidgetVisuals {
+                        rounding,
+                        bg_stroke,
+                        ..style.visuals.widgets.active
+                    },
+                    inactive: WidgetVisuals {
+                        rounding,
+                        bg_stroke,
+                        ..style.visuals.widgets.inactive
+                    },
+                    hovered: WidgetVisuals {
+                        rounding,
+                        ..style.visuals.widgets.hovered
+                    },
+                    noninteractive: WidgetVisuals {
+                        rounding,
+                        bg_stroke,
+                        ..style.visuals.widgets.noninteractive
+                    },
+                    ..style.visuals.widgets
+                },
+                window_rounding: rounding,
+                clip_rect_margin: 0.0,
+                ..style.visuals
+            },
+            spacing: Spacing {
+                item_spacing: (2.0, 2.0).into(),
+                window_margin: Margin::same(0.0),
+                ..style.spacing
+            },
+            ..style
+        });
 
-        if let Some(bg) = &butts.bg {
+        // set FG
+        if let Some(fg) = butts
+            .fg
+            .as_ref()
+            .map(|fg| colcon::hex_to_irgb(fg).ok())
+            .flatten()
+        {
             let style = cc.egui_ctx.style().as_ref().clone();
+            let fg = Color32::from_rgb(fg[0], fg[1], fg[2]);
             cc.egui_ctx.set_style(Style {
                 visuals: Visuals {
                     widgets: Widgets {
                         active: WidgetVisuals {
-                            bg_fill: Color32::BLACK,
+                            bg_fill: fg,
                             ..style.visuals.widgets.active
+                        },
+                        inactive: WidgetVisuals {
+                            fg_stroke: Stroke {
+                                width: 1.0,
+                                color: fg,
+                            },
+                            ..style.visuals.widgets.inactive
+                        },
+                        hovered: WidgetVisuals {
+                            fg_stroke: Stroke {
+                                width: 1.0,
+                                color: fg,
+                            },
+                            bg_stroke: Stroke {
+                                width: 1.0,
+                                color: fg,
+                            },
+                            ..style.visuals.widgets.hovered
+                        },
+                        noninteractive: WidgetVisuals {
+                            bg_fill: fg,
+                            ..style.visuals.widgets.noninteractive
+                        },
+                        ..style.visuals.widgets
+                    },
+                    ..style.visuals
+                },
+                ..style
+            });
+        }
+
+        // set BG
+        if let Some(bg) = butts
+            .bg
+            .as_ref()
+            .map(|bg| colcon::hex_to_irgb(bg).ok())
+            .flatten()
+        {
+            let style = cc.egui_ctx.style().as_ref().clone();
+            let bg = Color32::from_rgb(bg[0], bg[1], bg[2]);
+            cc.egui_ctx.set_style(Style {
+                visuals: Visuals {
+                    widgets: Widgets {
+                        active: WidgetVisuals {
+                            fg_stroke: Stroke {
+                                width: 1.0,
+                                color: bg,
+                            },
+                            ..style.visuals.widgets.active
+                        },
+                        inactive: WidgetVisuals {
+                            bg_fill: bg,
+                            bg_stroke: Stroke {
+                                width: 2.0,
+                                color: bg,
+                            },
+                            ..style.visuals.widgets.inactive
+                        },
+                        hovered: WidgetVisuals {
+                            bg_fill: bg,
+                            ..style.visuals.widgets.hovered
                         },
                         ..style.visuals.widgets
                     },
